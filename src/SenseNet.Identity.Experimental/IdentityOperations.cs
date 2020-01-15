9883b0d9d01caa2ccbc57534440d890d848287d5
tusmester <tusmester@gmail.com>
2020-01-15 17:39:53 +0100

    Validate credentials (#1)
    * Update sensenet references.
    * Add action for verifying a user's password.

---
+++ src/SenseNet.Identity.Experimental/IdentityOperations.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using SenseNet.ApplicationModel;
+using SenseNet.ContentRepository;
+using SenseNet.ContentRepository.Storage.Security;
+
+namespace SenseNet.Identity.Experimental
+{
+    public static class IdentityOperations
+    {
+        [ODataAction]
+        [ContentTypes(N.CT.PortalRoot)]
+        [AllowedRoles(N.R.All)]
+        public static async Task<object> ValidateCredentials(Content content, HttpContext context, string userName, string password)
+        {
+            if (context == null)
+                throw new ArgumentNullException(nameof(context));
+            if (userName == null)
+                throw new ArgumentNullException(nameof(userName));
+            if (password == null)
+                throw new ArgumentNullException(nameof(password));
+
+            if (!(context.RequestServices.GetService(typeof(UserManager<SnIdentityUser>)) is UserManager<SnIdentityUser> um))
+                throw new InvalidOperationException("User service not available.");
+
+            var user = await um.FindByNameAsync(userName);
+
+            if (user != null && await um.CheckPasswordAsync(user, password))
+            {
+                return new
+                {
+                    user.Id,
+                    user.Email,
+                    user.UserName
+                };
+            }
+
+            throw new SenseNetSecurityException("Invalid username or password.");
+        }
+    }
+}
